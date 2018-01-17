77b24056e548186f5bae562e3fdd27584291362c
homecat <szjacky2000@126.com>
2018-01-17 17:27:24 +0800

    commit file ...

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -8,7 +8,7 @@
 namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
-//use Zend\View\Model\ViewModel;
+use Zend\View\Model\ViewModel;
 
 class IndexController extends AbstractActionController
 {
