642db0e645e35138d16e2209cda49a6b71502795
homecat <szjacky2000@126.com>
2018-01-17 16:14:59 +0800

    commit file ...

---
+++ module/System/Module.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jk
+ * Date: 2018/1/17
+ * Time: 16:14
+ */
+namespace System;
+
+class Module
+{
+    public function getConfig()
+    {
+        return array();
+    }
+
+}
