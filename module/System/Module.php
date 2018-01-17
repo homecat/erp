22b0182475b5656ce1336a635c90194d7b169892
homecat <szjacky2000@126.com>
2018-01-17 23:13:44 +0800

    zhanghuaming ...

--- module/System/Module.php
+++ module/System/Module.php
@@ -13,7 +13,7 @@ class Module
     public function getConfig()
     {
         return array();
-//        return include __DIR__ . '/../config/module.config.php';
+        return include __DIR__ . '/../config/module.config.php';
     }
 
 }
