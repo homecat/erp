44b27d9dd39638bb6fb5321d771bb278e2218889
homecat <szjacky2000@126.com>
2018-01-18 09:21:25 +0800

    zhanghuaming ...

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -19,8 +19,8 @@ return [
                 'options' => [
                     'route'    => '/',
                     'defaults' => [
-                        'controller' => Controller\IndexController::class,
-                        'action'     => 'index',
+                        'controller' => Controller\UserController::class,
+                        'action'     => 'test',
                     ],
                 ],
             ],
