ac93ad92980f904ca50676d7eea00bd5e6593aaf
homecat <szjacky2000@126.com>
2018-01-25 10:22:06 +0800

    zhanghuaming ...

--- module/System/src/Controller/UserController.php
+++ module/System/src/Controller/UserController.php
@@ -8,36 +8,18 @@
 namespace System\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
-use Zend\View\Model\ViewModel;
-use Zend\View\Model\JsonModel;
 
 class UserController extends AbstractActionController
 {
     public function indexAction()
     {
         echo 'system index';
-//        $accounts = $this->getModel()->fetchAll();
-//        $data     = [];
-//        $total    = 0;
-//        foreach($accounts as $val){
-//            $val->StartDate = date('Y-m-d' , $val->StartDate);
-//            $val->Created   = date('Y-m-d' , $val->Created);
-//            $data[] = $val;
-//            $total ++;
-//        }
-//        $result =  new JsonModel(['total'=>$total , 'data'=>$data]);
-//        return $result;
+        exit;
     }
 
     public function testAction()
     {
-//        if ($this->getRequest()->isXmlHttpRequest()) {
-//            $jsonModel = new JsonModel(...);
-//
-//            /* @todo Render HTML script into `$html` variable, and add to `JsonModel` */
-//            return $jsonModel;
-//        } else {
-//            return new ViewModel(...);
-//        }
+        echo 'system test';
+        exit;
     }
 }
